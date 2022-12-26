a34d359e4cae39ddf8eed67431b4addddfbfd65e
Edward Yuabov <yuaboved@gmail.com>
2022-12-26 12:41:33 -0500

    initialized sequelize and made models

---
+++ models/user_diagnosis.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User_diagnosis extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User_diagnosis.init({
+    name: DataTypes.STRING,
+    possible_cause: DataTypes.STRING,
+    treatment: DataTypes.STRING,
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'User_diagnosis',
+  });
+  return User_diagnosis;
+};
